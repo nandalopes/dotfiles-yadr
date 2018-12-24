163912493ec653a673abeb24aa7da0e9a5018098
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2018-12-23 17:37:25 -0800

    Making `z` better with fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -12,3 +12,11 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
+# fasd & fzf change directory - jump using `fasd` if given argument, filter output of `fasd` using `fzf` else
+unalias z #removing fasd's alias for z first
+
+z() {
+  [ $# -gt 0 ] && fasd_cd -d "$*" && return
+  local dir
+  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
