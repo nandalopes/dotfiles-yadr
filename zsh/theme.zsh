94689751f15f189073f7440b172b9021c01c7880
Nanda Lopes <nandalopes@gmail.com>
2021-01-06 13:28:16 -0300

    fix(zsh) Fix `no prompt command` error on Ubuntu
    (cherry picked from commit 7f865704f6918563ba68ad7fe49070c9eeed789b)

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,1 +1,7 @@
-prompt skwp
+### fix linux prompt
+# Preview all with `prompt -p`
+# Options: adam2 giddie pure steeef elite2 elite restore redhat skwp suse
+
+if [[ $terminfo[colors] == 8 ]]; then
+  zstyle ':prezto:module:prompt' theme 'skwp'
+fi
