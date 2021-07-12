8959bbf2a911d9b9206c2759081d8113e9cd79af
Nanda Lopes <nandalopes@gmail.com>
2021-07-11 21:06:55 -0300

    docs(zsh): ubuntu with ansi keyboard

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -9,8 +9,8 @@ bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
 
-# Make numpad enter work
-bindkey -s "^[Op" "0"
-bindkey -s "^[Ol" "."
-bindkey -s "^[OM" "^M"
+# No need with ANSI configured in term
+# bindkey -s "^[Op" "0"
+# bindkey -s "^[Ol" "."
+# bindkey -s "^[OM" "^M"
 
